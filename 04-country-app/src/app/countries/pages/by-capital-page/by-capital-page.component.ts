a0fb92c32c77f4b4b3cb145bd042d436a34ea62c
Raúl Núñez <r4nu54@gmail.com>
2023-05-27 15:26:49 +0200

    feat: End of section 10 - Countries App.

--- 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-country-app/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,22 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 
 import { CountriesService } from '../../services/countries.service';
 
-import { Country } from '../../interfaces/country';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: [],
 })
-export class ByCapitalPageComponent {
-  placeholder: string = 'Buscar por capital';
-
+export class ByCapitalPageComponent implements OnInit {
   private countriesService = inject(CountriesService);
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
+  public initialValue: string = '';
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCapital.countries;
+    this.initialValue = this.countriesService.cacheStore.byCapital.term;
+  }
 
   searchByCapital(term: string): void {
-    this.countriesService.searchCapital(term).subscribe(countries => (this.countries = countries));
+    this.isLoading = true;
+    this.countriesService.searchCapital(term).subscribe(countries => {
+      this.countries = countries;
+      this.isLoading = false;
+    });
   }
 }
--- 04-country-app/src/app/shared/shared.module.ts
+++ 04-country-app/src/app/shared/shared.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, HomePageComponent, SideBarComponent, ContactPageComponent, SearchBoxComponent],
+  declarations: [HomePageComponent, LoadingSpinnerComponent, SearchBoxComponent, SideBarComponent],
   imports: [CommonModule, RouterModule],
-  exports: [AboutPageComponent, HomePageComponent, SideBarComponent, ContactPageComponent, SearchBoxComponent],
+  exports: [HomePageComponent, LoadingSpinnerComponent, SearchBoxComponent, SideBarComponent],
 })
 export class SharedModule {}
